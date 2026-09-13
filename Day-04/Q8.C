08809533aef7739e20e15ef08cf7461a0bcbd455
Prakhar <theprakharpratap@gmail.com>
2026-09-14 05:09:29 +0530

    Day 4 Solutions

---
+++ Day-04/Q8.C
@@ -0,0 +1,30 @@
+//Q8: Write a program to find and display the sum of the first n natural numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+Output 1:
+Sum=15
+
+Input 2:
+10
+Output 2:
+Sum=55
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+    printf("Enter the number : ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+
+    printf("Sum=%d", sum);
+
+    return 0;
+}
