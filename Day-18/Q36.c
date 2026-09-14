78d85cc43003ec4657e4194bdfe415de2309f820
Prakhar <theprakharpratap@gmail.com>
2026-09-14 05:59:10 +0530

    Day 18 Solutions

---
+++ Day-18/Q36.c
@@ -0,0 +1,33 @@
+//Q36: Write a program to find the HCF (GCD) of two numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+12 18
+Output 1:
+6
+
+Input 2:
+7 9
+Output 2:
+1
+
+*/
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    int x = a, y = b;
+    while (y != 0) {
+        int temp = y;
+        y = x % y;
+        x = temp;
+    }
+
+    printf("%d", x);
+
+    return 0;
+}
