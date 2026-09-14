7a39cd4622f72d37caf7ba16fca5cb6c3610a159
Prakhar <theprakharpratap@gmail.com>
2026-09-14 06:01:59 +0530

    Day 19 Solutions

---
+++ Day-19/Q37.c
@@ -0,0 +1,35 @@
+//Q37: Write a program to find the LCM of two numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+4 5
+Output 1:
+20
+
+Input 2:
+7 3
+Output 2:
+21
+
+*/
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    int x = a, y = b;
+    while (y != 0) {
+        int temp = y;
+        y = x % y;
+        x = temp;
+    }
+    int hcf = x;
+    int lcm = (a * b) / hcf;
+
+    printf("%d", lcm);
+
+    return 0;
+}
